633cf2307fe24fbe19158e920f4bf6b669fdcd6b
Rudy Faile <rudy_faile@redlands.edu>
2020-04-25 16:00:58 -0500

    macos: clean

--- clean.sh
+++ clean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 GAME="game"
 WEIRDMACFILE="game.dSYM"
+USELESSMACFILE=".DS_Store"
 
 echo "**Cleaning**"
 
-rm -f $GAME; rm -Rf $WEIRDMACFILE
+rm -f $GAME; rm -Rf $WEIRDMACFILE; rm -f $USELESSMACFILE 
 
-echo "**Cleaned $GAME and $WEIRDMACFILE**" 
+echo "**Removed  $GAME && $WEIRDMACFILE && $USELESSMACFILE**" 
