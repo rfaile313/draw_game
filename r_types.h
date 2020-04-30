2b55dce348737927ecc6f1c15832bef3fb6e44dd
Rudy Faile <febesbrasil@gmail.com>
2020-04-30 01:08:42 -0500

    more art

--- r_types.h
+++ r_types.h
@@ -12,23 +12,22 @@
 #include <string.h> //memset
 #include <float.h>  //FLT_MAX
 
-typedef uint8_t     u8;
-typedef int8_t      s8;
-typedef uint16_t    u16;
-typedef int16_t     s16;
-typedef uint32_t    u32;
-typedef int32_t     s32;
-typedef s32         b32;
-typedef uint64_t    u64;
-typedef int64_t     s64;
+typedef uint8_t     u8;  //0..255
+typedef int8_t      s8;  //-128..127
+typedef uint16_t    u16; //0..65,535
+typedef int16_t     s16; //-32,768..32,767
+typedef uint32_t    u32; //0..4,294,967,295
+typedef int32_t     s32; //-2,147,483,648..2,147,483,647
+typedef uint64_t    u64; //0 .. 18,446,744,073,709,551,615
+typedef int64_t     s64; //-9,223,372,036,854,775,808..9,223,372,036,854,775,807
 
-typedef float       f32;
-typedef double      f64;
+typedef float       f32; //	-3.4E38..3.4E38
+typedef double      f64; //	-1.7E308..1.7E308
 typedef const char* const_str;
 
-#define u32_max     UINT32_MAX
-#define s32_max     INT32_MAX
-#define f32_max     FLT_MAX
-#define f32_min     FLT_MIN
+#define u32_max     UINT32_MAX // 4,294,967,295
+#define s32_max     INT32_MAX  // 2,147,483,647
+#define f32_max     FLT_MAX    // 3.4E38
+#define f32_min     FLT_MIN    // -3.4E38
 
 #endif //R_TYPES_H
