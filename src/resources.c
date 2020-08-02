f869c0aa0696de2be5da45fbfc27f1b933b3be4a
Rudy Faile <febesbrasil@gmail.com>
2020-08-02 10:26:20 -0500

    Win:08/02/2020 10:25am. Building on Windows and Web - need to do a better job automating web build script and resources.c -- but works for now I guess \_()_/

--- src/resources.c
+++ src/resources.c
@@ -7,22 +7,22 @@
 const_str charTexturePath = "Resources/cowboyspenzilla/characters.png"; //128/4 x 256/8
 const_str tileTexturePath = "Resources/cowboyspenzilla/background.png";
 
-const_str enemy1IdleTexturePath = "C:/raylib/draw_game/Resources/cowboyspenzilla/enemy1idle.png";
-const_str xAnimationTexturePath = "C:/raylib/draw_game/Resources/rudy/xanimation.png";
-const_str bulletTexturePath = "C:/raylib/draw_game/Resources/rudy/bullet.png";
+const_str enemy1IdleTexturePath = "Resources/cowboyspenzilla/enemy1idle.png";
+const_str xAnimationTexturePath = "Resources/rudy/xanimation.png";
+const_str bulletTexturePath = "Resources/rudy/bullet.png";
 
-const_str soundBullet  = "C:/raylib/draw_game/Resources/sound/bullet.ogg";
-const_str soundDraw    = "C:/raylib/draw_game/Resources/sound/draw.ogg";
-const_str soundError   = "C:/raylib/draw_game/Resources/sound/error.ogg";
-const_str soundInitial = "C:/raylib/draw_game/Resources/sound/initial.ogg";
-const_str soundLose    = "C:/raylib/draw_game/Resources/sound/lose.ogg";
-const_str soundLoseWdl = "C:/raylib/draw_game/Resources/sound/losewithdelay.ogg";
-const_str soundOrgan   = "C:/raylib/draw_game/Resources/sound/organ.ogg";
-const_str soundShoot   = "C:/raylib/draw_game/Resources/sound/shoot.ogg";
-const_str soundWin     = "C:/raylib/draw_game/Resources/sound/win.ogg";
-const_str soundToggle  = "C:/raylib/draw_game/Resources/sound/togglesound.ogg";	
+const_str soundBullet  = "Resources/sound/bullet.ogg";
+const_str soundDraw    = "Resources/sound/draw.ogg";
+const_str soundError   = "Resources/sound/error.ogg";
+const_str soundInitial = "Resources/sound/initial.ogg";
+const_str soundLose    = "Resources/sound/lose.ogg";
+const_str soundLoseWdl = "Resources/sound/losewithdelay.ogg";
+const_str soundOrgan   = "Resources/sound/organ.ogg";
+const_str soundShoot   = "Resources/sound/shoot.ogg";
+const_str soundWin     = "Resources/sound/win.ogg";
+const_str soundToggle  = "Resources/sound/togglesound.ogg";	
 
-const_str fontAlagard  = "C:/raylib/draw_game/Resources/spritefont/custom_alagard.png";
+const_str fontAlagard  = "Resources/spritefont/custom_alagard.png";
 
 /*
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
@@ -46,8 +46,6 @@ const_str soundWin     = "Resources/sound/win.ogg";
 const_str soundToggle  = "Resources/sound/togglesound.ogg";	
 
 const_str fontAlagard  = "Resources/spritefont/custom_alagard.png";
-
-
 
 #elif __APPLE__
 //not sure what this is V
@@ -92,4 +90,5 @@ const_str fontAlagard  = "../Resources/spritefont/custom_alagard.png";
 #else
 #   error "Unknown compiler"
 #endif
+
 */
